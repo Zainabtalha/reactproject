485825f88abd23cac2cb8a11451156b5002b7a83
Zainab Talha <133660467+Zainabtalha@users.noreply.github.com>
2024-06-28 22:05:09 +0500

    Add files via upload

---
+++ reactproject/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div>
+<h1>Hello World</h1>
+<p>Hi I am Zainab and this is my first react project</p>
+<button>Click me</button>
+<img src="/vite.svg"/>
+
+    </div>
+  )
+}
+
+export default App
